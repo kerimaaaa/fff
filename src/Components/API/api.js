b554706c48f8ead4a402bdea0b48a6538a32e085
kerimaaaa <119394857+kerimaaaa@users.noreply.github.com>
2023-06-10 15:07:44 +0300

    Update api.js

--- src/Components/API/api.js
+++ src/Components/API/api.js
@@ -1,6 +1,39 @@
-export const baseURL = 'http://35.242.202.126/api/';
+import axios from "axios";
 
-
-export const createUser = (user) => {
-    return baseURL.post('/api/register/email',user)
+const instance = axios.create({
+  baseURL: 'http://35.242.202.126/api/',
+  headers: {
+    'Content-Type': 'application/json',
   }
+})
+
+export const getApi = {
+  setToken(token) {
+    instance.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+  },
+  login(username, pass) {
+    return instance.post(`token/`, {
+      "Username": username,
+      "Password": pass
+    })
+  },
+  registerEmail(data) {
+    return instance.post(`register/email/`, {
+      "Email": data.email,
+    })
+  },
+  registerAddinfo(data) {
+    return instance.put(`register/`, {
+      "Username": data.username,
+      "Name": data.firstName,
+      "Surname": data.lastName,
+      "PhoneNumber": data.phoneNumber
+    })
+
+  },
+  registerPassword(password) {
+    return instance.put(`/register/set_password/`, {
+      "Password": password
+    })
+  }
+}
